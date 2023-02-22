5ba27ae2eee6dc80f5ee037b0b6d576686874f58
emanuelbrits <107653566+emanuelbrits@users.noreply.github.com>
2023-02-22 16:35:31 -0300

    Delete q01.py

--- Prog Int/q01.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-response = requests.get('https://www.google.com/')
-soup = BeautifulSoup(response.text, 'html.parser')
-
-links = soup.find_all('a')
-for link in links:
-    print(link.get('href'))
