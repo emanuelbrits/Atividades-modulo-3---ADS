a3c3ee2b1bd4ef83b98298e4854f4e7ed9c4acb0
emanuelbrits <107653566+emanuelbrits@users.noreply.github.com>
2023-02-22 16:35:58 -0300

    Delete q04.py

--- Prog Int/q04.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-response = requests.get("https://www.lance.com.br/files/article_main/uploads/2023/02/18/63f129c412027.jpeg")
-with open('aviao.jpeg', 'wb') as f:
-    f.write(response.content)
