c97f395d8e23a5ad0f18a8182bff0ee1dff6f7df
emanuelbrits <107653566+emanuelbrits@users.noreply.github.com>
2023-03-03 13:12:01 -0300

    Update buscador.py

--- Prog Int/atividade 2/buscador.py
+++ Prog Int/atividade 2/buscador.py
@@ -1,35 +1,48 @@
 from bs4 import BeautifulSoup
 import requests
+import requests_cache
 
-url = 'https://ge.globo.com/'
+requests_cache.install_cache('cache')
 
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')    
+def search(termo, url, profundidade): 
 
-text = soup.get_text()
-termo = input('Termo a ser buscado: ')
-profundidade = int(input('Profundidade de busca: '))
+    if profundidade < 0:
+        return
 
-def search(termo, texto, profundidade, soup): 
-    if termo in texto and profundidade == 0:        
-        index_inicio = max(texto.index(termo) - 20, 0)        
-        index_fim = min(texto.index(termo) + len(termo) + 20, len(texto))        
-        resultado = texto[index_inicio:index_fim]     
-        return resultado
-    else:
-        if profundidade > 0:      
-            links = soup.find_all('a')        
-            for link in links:            
-                href = link.get('href')  
-                response = requests.get(href)
-                soup = BeautifulSoup(response.content, 'html.parser')  
-                text = soup.get_text()          
-                if termo in text:        
-                    index_inicio = max(text.index(termo) - 20, 0)        
-                    index_fim = min(text.index(termo) + len(termo) + 20, len(text))        
-                    resultado = text[index_inicio:index_fim]     
-                    return resultado
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
+    visitados = []
+    links = []
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href and href.startswith(('http')):
+            links.append(href)
+            if href not in visitados:
+                visitados.append(href)
+                search(termo, href, profundidade-1)
 
+    text = soup.get_text().lower()
 
-print(search(termo, text, profundidade, soup))
+    if termo.lower() in text.lower():
+        index_inicio = max(text.lower().index(termo.lower()) - 20, 0)
+        index_fim = min(text.lower().index(termo.lower()) + 20, len(text))
+        print(f"Termo encontradao em {url}: {text[index_inicio:index_fim].strip()}")
+        print("")
+
+    referencias = 0
+    maior_referencia = 0
+    link_maior_referencia = ''
+
+    for link in links:
+        if url in link:
+            referencias += 1
+    
+    if referencias > maior_referencia:
+        link_maior_referencia = ''
+        link_maior_referencia+= url
+
+    return link_maior_referencia
+
+aux = search("torneio", "https://www.rocketleague.com/pt-br//", 1)
+print(f"link com mais referências {aux}")
